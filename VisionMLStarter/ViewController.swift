cddf38fce0831ceaefbf6c97594b58f31b41f731
Chris Longe <chris.longe@capitalone.com>
2017-08-13 16:40:02 -0400

    Initial Commit

---
+++ VisionMLStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionMLStarter
+//
+//  Created by Longe, Chris on 8/13/17.
+//  Copyright © 2017 Longe, Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
