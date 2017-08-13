8c47809bfb5be5f70141ada71b4ff711f22a268e
Chris Longe <chris.longe@capitalone.com>
2017-08-13 18:22:24 -0400

    Adds hotdog asset and iboutlets mark

--- VisionMLStarter/ViewController.swift
+++ VisionMLStarter/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - IBOutlets
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
